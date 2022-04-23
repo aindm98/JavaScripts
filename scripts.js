1e0f88ed430f5a2b2db0ad82810622442704c711
Aindrila Mukherjee <64476010+aindm98@users.noreply.github.com>
2022-04-23 15:03:45 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,81 @@
+// 1. const num1 = parseInt(prompt('num1:  '));   `                  20
+//    const num2 = parseInt(prompt('num2:  '));                      40
+//    const sum = num1 + num2;
+//    console.log(`The sum of ${num1} and ${num2} is ${sum}`);       60
+
+// 2.  let P = 100, R = 6 , T = 2;
+//     let SI = (P * T * R) / 100;
+//     console.log("Simple Interest = " + SI);                      Simple Interest: 12
+
+// 3.   let m = parseInt(prompt('m: '));                            12
+//      let v= parseInt(prompt('v'));
+//      let k = 0.5 * m* v * v;                                      34
+//      console.log('Kinetic Energy: '+ k);                          Kinetic Energy: 6936
+
+// 4.      const Fahrenheit = 56;
+//         const Celsius = ((Fahrenheit - 32)* 5)/9;
+//          console.log('Celsius = '+Celsius);                      Celsius =  13.333333334
+
+// 5.  let a = parseInt(prompt('a: '));                              2
+//      function areaCube(a){
+//           return (a*a);                   
+//      }  
+//      function perimeterCube(a){
+//          return ( 4*a);
+//      }
+//     function surfaceareaCube(a){
+//          return (6*a*a);
+//      }
+//       function volumeCube(a){
+//          return (a*a*a);
+//      }
+//       console.log('Area: '+ areaCube(a));                          Area: 4
+//       console.log('Perimater: ' + perimeterCube(a));               Perimater: 8
+//      console.log('Surface Area: '+surfaceareaCube(a));             Surface Area: 24
+//      console.log('Volume: ' +volumeCube(a));                       Volume: 8
+
+// 6.      let cp = parseInt(prompt('CP: '))                                     
+//         let sp = parseInt(prompt('SP: '))
+//          function Profit(cp, sp)
+//            {
+//              let profit = (sp - cp);
+//               return profit;
+//            }
+//          function Loss(cp, sp)
+//            {
+//              let Loss = (cp - sp);
+//              return Loss;
+//            } 
+//            if (sp == cp){
+//                  console.log("No profit nor Loss");                            
+//            } else if {(sp > cp)
+//                  console.log(Profit(cp, sp) + " Profit ");       CP = 1500, SP = 2000    500 Profit
+//             } else{
+//                  console.log(Loss(cp, sp) + " Loss ");            CP = 3125, SP = 1125   2000 Loss
+//            }
+
+// 7.    const number = parseInt(prompt('Enter a positive integer: '));     Enter a positive integer: 100
+//      let sum = 0;
+//      for (let i = 1; i <= number; i++) {
+//           sum += i;
+//      }
+//          console.log('The sum of natural numbers:', sum);                The sum of natural numbers: 5050
+
+//   8.   let n = parseInt(prompt("Enter a number:"));                       Enter a number: 4
+//          for (i = n - 1; i >= 0; i--) {                                   Descending Order: 7
+//              a = 1 + i * 2;                                                                 5
+//              console.log('Descending Order: +a);                                            3
+//          }                                                                                  1
+ 
+// 10.      let n=32243;
+//          let reverse=0;
+//          let rem; 
+//          while(n!=0)    
+//          {    
+//               rem=n%10;    
+//               reverse=reverse*10+rem;    
+//               n=Math.floor(n/10)
+//           }   
+//           console.log("Reverse Number: "+ reverse)               Reverse Number:     34223
+
+
